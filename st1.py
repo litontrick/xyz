6a2e5172a6e352826422372bdbf002809d934cc2
litontrick <litonsaha121@gmail.com>
2025-05-15 10:35:07 +0530

    Add files via upload

---
+++ st1.py
@@ -0,0 +1,7 @@
+import streamlitas st
+st.title("My first Streamlit App")
+st.write("Hello guys, witness the origin of Age of Power")
+st.subheader("Image: ")
+st.image("Animals.jpg")
+st.subheader("Audio: ")
+st.audio("back.mp3")
